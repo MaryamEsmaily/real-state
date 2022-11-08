99437a1a225aaff0203c7c885e18edf10f8bc5fb
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2022-11-08 16:56:33 +0330

    update filter section

--- container/app/dashboard/FilterSection/index.jsx
+++ container/app/dashboard/FilterSection/index.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import KeyboardArrowUpRoundedIcon from "@mui/icons-material/KeyboardArrowUpRounded";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import { Stack } from "@mui/system";
 
-function FilterSection() {
-  return <div>FilterSection</div>;
+export default function FilterSection() {
+  //
+  const [open, setOpen] = React.useState(false);
+  //
+  return (
+    <Stack height="calc(100vh - 100px)" justifyContent="space-between" mx={1}>
+      <Box>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={2}
+        >
+          <Typography>استان</Typography>
+          <IconButton onClick={() => setOpen((prev) => !prev)}>
+            {open ? (
+              <KeyboardArrowUpRoundedIcon />
+            ) : (
+              <KeyboardArrowDownRoundedIcon />
+            )}
+          </IconButton>
+        </Stack>
+
+        <Collapse in={open} collapsedSize={0}>
+          <FormControlLabel
+            control={<Checkbox size="small" defaultChecked />}
+            label="مازندران"
+          />
+        </Collapse>
+      </Box>
+      <Button fullWidth size="small">
+        جستجو
+      </Button>
+    </Stack>
+  );
 }
-
-export default React.memo(FilterSection);
