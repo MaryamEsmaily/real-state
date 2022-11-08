787cebb524830d6cb7be650d68405d903c95ff37
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2022-11-08 15:21:26 +0330

    add dashboards component

---
+++ container/app/dashboard/AdvertisingSection/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AdvertisingSection() {
+  return <div>AdvertisingSection</div>;
+}
+
+export default React.memo(AdvertisingSection);
--- pages/app/dashboard.js
+++ pages/app/dashboard.js
@@ -1,8 +1,27 @@
+import { Box, Grid } from "@mui/material";
 import AppLayout from "layout/AppLayout";
+import dynamic from "next/dynamic";
 import React from "react";
-
+// dynamic import
+const AdvertisingSection = dynamic(() =>
+  import("container/app/dashboard/AdvertisingSection")
+);
+const FilterSection = dynamic(() =>
+  import("container/app/dashboard/FilterSection")
+);
 function DashboardPage() {
-  return <div>Dashboard</div>;
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <FilterSection />
+        </Grid>
+        <Grid item xs={10}>
+          <AdvertisingSection />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 DashboardPage.getLayout = (page) => <AppLayout>{page}</AppLayout>;
